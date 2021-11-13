e843c61e2163b9a680f5ceec874c5f778e97baa3
Dinesh1956 <dinesharsole02@gmail.com>
2021-11-13 12:18:58 +0530

    PUSH by DSP

---
+++ src/test/java/org/hcl/Dinesh.java
@@ -0,0 +1,17 @@
+package org.hcl;
+
+public class Dinesh {
+
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+}
